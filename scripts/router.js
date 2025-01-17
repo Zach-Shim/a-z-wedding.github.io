19b0a77dff96362cd2f941b24787ada8797b6a19
Zach Shim <shimz2@uw.edu>
2025-01-16 19:57:18 -0700

    Initial upload.

---
+++ scripts/router.js
@@ -0,0 +1,22 @@
+class Router
+{
+    static routes = {
+        "Home": "index.html",
+        "About Us": "about.html",
+        "Photos": "photos.html",
+        "Wedding": "wedding.html",
+        "Schedule": "schedule.html",
+        "RSVP": "rsvp.html",
+    };
+
+    static AddRoute(name, path)
+    {
+        return Router.routes[name] = path;
+    }
+
+    static GetRoute(name)
+    {
+        return Router.routes[name];
+    }
+
+}
